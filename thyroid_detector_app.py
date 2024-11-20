f7faed8fd61c117f11041371aa4c274b1a735c2a
rakshita2702 <163335070+rakshita2702@users.noreply.github.com>
2024-11-20 23:08:27 +0530

    Update thyroid_detector_app.py

--- thyroid_detector_app.py
+++ thyroid_detector_app.py
@@ -5,7 +5,7 @@ import numpy as np
 # Load the saved model
 @st.cache_resource
 def load_model():
-    with open('best_model (1).pkl', 'rb') as f:
+    with open('best_model_1.pkl', 'rb') as f:
         return pickle.load(f)
 
 model = load_model()
